70d75683ec690573b063c00782426b6a98811cb0
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2020-11-02 23:52:24 +0000

    Implemented PhaseSpaceParameterisation and wrote fitting program using this library However, compilation has not yet been attempted

--- apps/Fitting.cpp
+++ apps/Fitting.cpp
@@ -3,15 +3,35 @@
  * Fitting is the program for doing the binned fitting
  */
 
-#include<Bin.h>
+#include<string>
 #include<iostream>
+#include"PhaseSpaceParameterisation.h"
+#include"TTree.h"
+#include"BinList.h"
+#include"DDecayParameters.h"
+#include"CPParameters.h"
 
 int main() {
-  Bin money;
-  int a = 35, b = 5;
-  money.AddPennies(a);
-  money.AddPounds(b);
-  double amount = money.GetMoney();
-  std::cout << amount << std::endl;
+  std::string Bplusfile("/data/lhcb/users/tat/D02KKpipi/BplusEvents/BplustoDK_1K.root");
+  std::string Bminusfile("/data/lhcb/users/tat/D02KKpipi/BminusEvents/BminustoDK_1K.root");
+  PhaseSpaceParameterisation psp;
+  TFile fBplus(Bplusfile, "READ");
+  TFile fBminus(Bminusfile, "READ");
+  TTree *treeBplus = (*TTree)fBplus.GetObject("DalitzEventList");
+  TTree *treeBminus = (*TTree)fBminus.GetObject("DalitzEventList");
+  BinList binlist(psp);
+  binlist.LoadTree(treeBplus, +1);
+  binlist.LoadTree(treeBminus, -1);
+  double masses[4] = {0.493677, 0.493677, 0.13957039, 0.13957039};
+  DDecayParameters ddparameters(psp, 1.86483, masses, 1000);
+  Fitter fit(binlist, ddparameters);
+  CPParameters cpparameters(0.1, 0.1, 0.1, 0.1);
+  double xplus, xminus, yplus, yminus;
+  fit.DoFit(cpparameters);
+  cpparameters.GetCPParameters(xplus, xminus, yplus, yminus);
+  std::cout << "xplus = " << xplus << std::endl;
+  std::cout << "xminus = " << xminus << std::endl;
+  std::cout << "yplus = " << yplus << std::endl;
+  std::cout << "yminus = " << yminus << std::endl;
   return 0;
 }
---
+++ src/PhaseSpaceParameterisation.cpp
@@ -0,0 +1,34 @@
+// Martin Duy Tat 2nd November 2020
+
+#include"PhaseSpaceParameterisation.h"
+#include"Event.h"
+
+PhaseSpaceParameterisation::PhaseSpaceParameterisation(): m_bins(8) {
+}
+
+int PhaseSpaceParameterisation::WhichBin(const Event &event) {
+  double s02 = event.GetInvMass(0, 2);
+  double s13 = event.GetInvMass(1, 3);
+  double s012 = event.GetInvMass(0, 1, 2);
+  if(s02 < 0.8 && s13 < 0.8 && s012 < 1.5) {
+    return 0;
+  } else if(s02 >= 0.8 && s13 < 0.8 && s012 < 1.5) {
+    return 1;
+  } else if(s02 < 0.8 && s13 >= 0.8 && s012 < 1.5) {
+    return 2; 
+  } else if(s02 < 0.8 && s13 < 0.8 && s012 >= 1.5) {
+    return 3;
+  } else if(s02 >= 0.8 && s13 >= 0.8 && s012 < 1.5) {
+    return 4;
+  } else if(s02 >= 0.8 && s13 < 0.8 && s012 >= 1.5) {
+    return 5;
+  } else if(s02 < 0.8 && s13 >= 0.8 && s012 >= 1.5) {
+    return 6;
+  } else {
+    return 7;
+  }
+}
+
+int PhaseSpaceParameterisation::NumberOfBins() {
+  return m_bins;
+}
