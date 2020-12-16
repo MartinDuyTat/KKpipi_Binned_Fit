5da5941b8adaeec778aee85d536da403941d54e2
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2020-12-16 00:27:27 +0000

    Optmized the binning scheme by using more bins in regions with larger interference

--- src/AmplitudePhaseSpace.cpp
+++ src/AmplitudePhaseSpace.cpp
@@ -7,6 +7,7 @@
 #include"AmplitudePhaseSpace.h"
 #include"Amplitude.h"
 #include"Event.h"
+#include"Constants.h"
 
 AmplitudePhaseSpace::AmplitudePhaseSpace(const int &bins): PhaseSpaceParameterisation(bins) {
 }
@@ -17,7 +18,28 @@ AmplitudePhaseSpace::~AmplitudePhaseSpace() {
 int AmplitudePhaseSpace::WhichBin(const Event &event) const {
   std::vector<double> EventVector = event.GetEventVector();
   double phase = std::arg(m_amplitude(EventVector, +1)*std::conj(m_amplitude(EventVector, -1)));
-  return static_cast<int>((phase + TMath::Pi())/(2*TMath::Pi()/NumberOfBins()));
+  phase -= KKpipi_Constants::gamma - TMath::Pi()/6;
+  if(phase < -TMath::Pi()) {
+    phase += 2*TMath::Pi();
+  }
+  if(phase < -7*TMath::Pi()/9) {
+    return 0;
+  } else if(phase >= -7*TMath::Pi()/9 && phase < -5*TMath::Pi()/9) {
+    return 1;
+  } else if(phase >= -5*TMath::Pi()/9 && phase < -TMath::Pi()/3) {
+    return 2;
+  } else if(phase >= -TMath::Pi()/3 && phase < -TMath::Pi()/6) {
+    return 3;
+  } else if(phase >= -TMath::Pi()/6 && phase < 0.0) {
+    return 4;
+  } else if(phase >= 0.0 && phase < TMath::Pi()/3) {
+    return 5;
+  } else if(phase >= TMath::Pi()/3 && phase < 2*TMath::Pi()/3) {
+    return 6;
+  } else {
+    return 7;
+  }
+  //return static_cast<int>((phase + TMath::Pi())/(2*TMath::Pi()/NumberOfBins()));
 } 
 
 int AmplitudePhaseSpace::NumberOfBins() const {
