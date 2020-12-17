6cbc161805ed601aa64bb1c740dbbec77e4024b1
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2020-12-17 17:24:32 +0000

    Tried optimizing the bins

--- src/AmplitudePhaseSpace.cpp
+++ src/AmplitudePhaseSpace.cpp
@@ -18,23 +18,17 @@ AmplitudePhaseSpace::~AmplitudePhaseSpace() {
 int AmplitudePhaseSpace::WhichBin(const Event &event) const {
   std::vector<double> EventVector = event.GetEventVector();
   double phase = std::arg(m_amplitude(EventVector, +1)*std::conj(m_amplitude(EventVector, -1)));
-  phase -= KKpipi_Constants::gamma - TMath::Pi()/6;
-  if(phase < -TMath::Pi()) {
-    phase += 2*TMath::Pi();
+  phase += KKpipi_Constants::gamma;
+  if(phase > TMath::Pi()) {
+    phase -= 2*TMath::Pi();
   }
-  if(phase < -7*TMath::Pi()/9) {
-    return 0;
-  } else if(phase >= -7*TMath::Pi()/9 && phase < -5*TMath::Pi()/9) {
-    return 1;
-  } else if(phase >= -5*TMath::Pi()/9 && phase < -TMath::Pi()/3) {
-    return 2;
-  } else if(phase >= -TMath::Pi()/3 && phase < -TMath::Pi()/6) {
-    return 3;
-  } else if(phase >= -TMath::Pi()/6 && phase < 0.0) {
+  if(phase < 0.0) {
+    return static_cast<int>((phase + TMath::Pi())/(TMath::Pi()/4.0));
+  } else if(phase < TMath::Pi()/6) {
     return 4;
-  } else if(phase >= 0.0 && phase < TMath::Pi()/3) {
+  } else if(phase < KKpipi_Constants::gamma) {
     return 5;
-  } else if(phase >= TMath::Pi()/3 && phase < 2*TMath::Pi()/3) {
+  } else if(phase < 2*KKpipi_Constants::gamma - 0.3) {
     return 6;
   } else {
     return 7;
