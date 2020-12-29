0eb2f9e74dbc664f508fbbf3d8056296fa429f5b
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2020-12-29 10:19:05 +0000

    Improved the amplitude binning scheme by accounting for the magnitude of the amplitudes as well

--- src/AmplitudePhaseSpace.cpp
+++ src/AmplitudePhaseSpace.cpp
@@ -21,18 +21,21 @@ void AmplitudePhaseSpace::ReadAmplitudeFromEvent(bool TrueIfReadFromEvent) {
 
 int AmplitudePhaseSpace::WhichBin(const Event &event) const {
   std::vector<double> EventVector = event.GetEventVector();
-  double phase;
-  if(m_ReadAmplitudeFromEvent) {
-    std::complex<double> D_amplitude, Dbar_amplitude;
+  std::complex<double> D_amplitude, Dbar_amplitude;
+  double phase, rD;
+  if(m_ReadAmplitudeFromEvent) {    
     event.GetAmplitudes(D_amplitude, Dbar_amplitude);
-    phase = std::arg(D_amplitude*std::conj(Dbar_amplitude));
   } else {
-    phase = std::arg(m_amplitude(EventVector, +1)*std::conj(m_amplitude(EventVector, -1)));
+    D_amplitude = m_amplitude(EventVector, +1);
+    Dbar_amplitude = m_amplitude(EventVector, -1);
   }
-  if(phase > 0) {
-    return static_cast<int>(phase/(TMath::Pi()/NumberOfBins())) + 1;
+  phase = std::arg(D_amplitude*std::conj(Dbar_amplitude));
+  rD = std::norm(D_amplitude/Dbar_amplitude);
+  int BinNumber = static_cast<int>((phase + TMath::Pi())/(2*TMath::Pi()/NumberOfBins())) + 1;
+  if(rD < 1) {
+    return BinNumber;
   } else {
-    return -(static_cast<int>(-phase/(TMath::Pi()/NumberOfBins())) + 1);
+    return -(NumberOfBins() + 1 - BinNumber);
   }
 } 
 
